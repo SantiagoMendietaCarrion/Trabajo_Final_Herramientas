fa62589bf226bb623336bf38178f1814c1a64be2
Santiago Mendieta Carrión <samendieta@utpl.edu.ec>
2024-05-18 21:10:05 -0500

    'Cambio'

--- SQL/Insertar_Datos.sql
+++
@@ -1,8 +0,0 @@
-use PRIMERA;
-show tables;
-insert into persona
-(NOMBRE, APELLIDO, DNI, NACIMIENTO)
-value
-("Xevi", "Casado", "234567", "19900902");
-
-select *from persona;
--- SQL/Primera_Base_Datos.sql
+++
@@ -1,8 +0,0 @@
-#La siguiente linea de codigo sirve para crear nuestra primera base de datos
-CREATE DATABASE PRIMERA;
-#Esta es la segunda base de datos
-CREATE DATABASE SEGUNDA;
-SHOW DATABASES;
-/*
-Comentario adicional
-*/
